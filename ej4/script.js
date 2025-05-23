ac162deffae8d7c80109417505c672f2a1904f0b
Dezt1an <cristianexefer26@gmail.com>
2025-05-23 19:59:15 -0300

    ej4 agregado a la rama ejercicios

---
+++ ej4/script.js
@@ -0,0 +1,7 @@
+function calcular(){
+    const peso = parseFloat (document.getElementById ("peso").value)
+    const altura = parseFloat (document.getElementById ("altura").value)
+
+    const total= peso/(altura * altura)
+    alert(total)
+}
